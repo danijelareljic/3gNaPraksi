32304a87435171aaea456e4f1fd7230e5944cc78
danijelareljic <44097308+danijelareljic@users.noreply.github.com>
2019-05-28 21:05:23 +0200

    slajder na klik

--- main.js
+++ main.js
@@ -10,4 +10,8 @@ $(document).ready(function(){
     function(){
       $("#slajd").carousel("pause");
     });
+
+   $("#carouselExampleControls").click(function(){
+     $("#carouselExampleControls").carousel("next");
+   }); 
   });
